99b909bb2fe05fb0b054c0af3f36d8370976e8bf
leonct74 <19290124+leonct74@users.noreply.github.com>
2020-08-03 15:34:31 +0200

    split code into a library

---
+++ libs/availability.js
@@ -0,0 +1,116 @@
+import moment from "moment";
+// Gets an array of all active day od the week and filter those not active
+// const getDeliveryActives = openingHours
+//   .map((item, i) => (item.active ? i : null))
+//   .filter(elem => elem !== null);
+
+// Creates an array of 1 hour timeslot from a time rage
+function getTimeDiff(startTime, endTime, day) {
+  const timeS = startTime; // "09:00"
+  const timeE = endTime; // "18:00"
+  const [hoursS, minutesS] = timeS.split(":");
+  const [hoursE, minutesE] = timeE.split(":");
+  //const startTFirst = moment(today.set({ hours: hoursS, minutes: 0 }));
+  const startT = moment(moment(day).set({ hours: hoursS, minutes: minutesS }));
+  const endT = moment(moment(day).set({ hours: hoursE, minutes: minutesE }));
+  console.log("startT and endT are: ", startT, endT);
+
+  // Interval between the start and end time
+  let duration = endT.diff(startT, "hours");
+  console.log("duration is: ", duration);
+
+  let timeSlots = [];
+  for (let i = 0; i < duration; i++) {
+    // Moment add function mutates startT and therefore I need to be careful
+    // Renders the slot adding the needed minute to start time to end the slot at 00 minutes of the next hours
+    timeSlots.push(
+      `${startT.format("LT")} - ${startT
+        .add({ minutes: 60 - minutesS })
+        .format("LT")}`
+    );
+  }
+
+  console.log("timeSlots is: ", timeSlots);
+  return timeSlots;
+}
+
+// Return an array of dates ranges in the future corresponding to a specified array of days of the week
+// Accept nWeeks param which is used to know which week in the future to use for calculating the date
+// For instance if we want to know the dates of the second week after the current, nWeek will be 2
+const getOneWeekAvailableDates = async (sourceArr, nWeeks, fulfillmentTime) => {
+  const orderDay = moment(Date.now()).add(fulfillmentTime, "h");
+  const todayDay = orderDay.date(); // get current day integer value of the month from date
+  /* currentDayOfWeek needed to prevent rendering an earlier dayoftheWeek.
+  For example, without it if we are on Tuesday, the previous Sunday and Monday would be
+  rendered */
+  const currentDayOfWeek = moment(orderDay).isoWeekday();
+
+  return sourceArr
+    .map((item, i) => {
+      // Use js Intl.DateTimeFormat to get the right date format
+      // and moment to get UTC date in millisecons
+      if (item.active && i >= currentDayOfWeek) {
+        const day = new Intl.DateTimeFormat("en-US", {
+          weekday: "long",
+          // year: "numeric",
+          month: "long",
+          day: "2-digit"
+        }).format(
+          moment(orderDay).day(i + nWeeks * 7) // moment().day(24); // 3 Wednesdays from now (3 + 7 + 7 + 7)
+          // .format("dddd, MMMM Do YYYY LT")
+        );
+
+        return {
+          locations: item.locations,
+          weekDays: day,
+          // If is current day we need start time to account the Fulfillment time
+          timeSlots: item.times.map((tim, t) => {
+            let dayDay = orderDay.date(); // day Integer value
+            let isCurrentDay = todayDay - dayDay;
+            const currentHours = moment(orderDay).hours(); // HERE WE ADD FULFILLMENT TIME
+            const currentMinutes = moment(orderDay).minutes();
+            const [startTimeHours, minutesS] = tim.startTime.split(":");
+
+            // IMPORTANT: Below condition is needed to start rendering only the current day of the week
+            // from the current hour. The condition "currentHours > startTimeHours" instead, is needed to prevent
+            // the first time slot to be rendered twice when times array contains more than one timeslot/obj.
+            // Infact this iteation is called for each timeslot/obj in times array.
+            if (
+              isCurrentDay === 0 &&
+              i === currentDayOfWeek &&
+              currentHours > startTimeHours
+            ) {
+              return getTimeDiff(
+                `${currentHours}:${currentMinutes}`,
+                tim.endTime,
+                day
+              );
+            } else {
+              return getTimeDiff(tim.startTime, tim.endTime, day);
+            }
+          })
+        };
+      }
+      return null;
+    })
+    .filter(elem => elem !== null); // Filter all undefined element due to active being false
+};
+
+// .add(15 * i, 'minutes')
+// .format('hh:mm A');
+const getMultipleWeeksAvailability = async (
+  sourceArr,
+  nWeeks,
+  fulfillmentTime
+) => {
+  let arr = [];
+  let newArr = [];
+  for (let i = 0; i <= nWeeks; i++) {
+    newArr = await getOneWeekAvailableDates(sourceArr, i, fulfillmentTime);
+    console.log("NEWARR: ", newArr);
+    arr = [...arr, ...newArr];
+  }
+  return await arr;
+};
+
+export default getMultipleWeeksAvailability;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Fragment, useEffect, useState } from "react";
-import moment from "moment";
+// import moment from "moment";
 import Typography from "@material-ui/core/Typography";
 import "./styles.css";
-
-const FulfillmentTime = 0;
+// CUSTOM LIB
+import getMultipleWeeksAvailability from "../libs/availability";
 
 const openingHours = [
   {
@@ -291,151 +291,133 @@ const deliveryHours = [
   }
 ];
 
-// Gets an array of all active day od the week and filter those not active
-// const getDeliveryActives = openingHours
-//   .map((item, i) => (item.active ? i : null))
-//   .filter(elem => elem !== null);
+// // Gets an array of all active day od the week and filter those not active
+// // const getDeliveryActives = openingHours
+// //   .map((item, i) => (item.active ? i : null))
+// //   .filter(elem => elem !== null);
 
-// Creates an array of 1 hour timeslot from a time rage
-function getTimeDiff(startTime, endTime, day) {
-  const timeS = startTime; // "09:00"
-  const timeE = endTime; // "18:00"
-  const [hoursS, minutesS] = timeS.split(":");
-  const [hoursE, minutesE] = timeE.split(":");
-  //const startTFirst = moment(today.set({ hours: hoursS, minutes: 0 }));
-  const startT = moment(moment(day).set({ hours: hoursS, minutes: minutesS }));
-  const endT = moment(moment(day).set({ hours: hoursE, minutes: minutesE }));
-  console.log("startT and endT are: ", startT, endT);
+// // Creates an array of 1 hour timeslot from a time rage
+// function getTimeDiff(startTime, endTime, day) {
+//   const timeS = startTime; // "09:00"
+//   const timeE = endTime; // "18:00"
+//   const [hoursS, minutesS] = timeS.split(":");
+//   const [hoursE, minutesE] = timeE.split(":");
+//   //const startTFirst = moment(today.set({ hours: hoursS, minutes: 0 }));
+//   const startT = moment(moment(day).set({ hours: hoursS, minutes: minutesS }));
+//   const endT = moment(moment(day).set({ hours: hoursE, minutes: minutesE }));
+//   console.log("startT and endT are: ", startT, endT);
 
-  // Interval between the start and end time
-  let duration = endT.diff(startT, "hours");
-  console.log("duration is: ", duration);
+//   // Interval between the start and end time
+//   let duration = endT.diff(startT, "hours");
+//   console.log("duration is: ", duration);
 
-  let timeSlots = [];
-  for (let i = 0; i < duration; i++) {
-    // Moment add function mutates startT and therefore I need to be careful
-    // Renders the slot adding the needed minute to start time to end the slot at 00 minutes of the next hours
-    timeSlots.push(
-      `${startT.format("LT")} - ${startT
-        .add({ minutes: 60 - minutesS })
-        .format("LT")}`
-    );
-  }
+//   let timeSlots = [];
+//   for (let i = 0; i < duration; i++) {
+//     // Moment add function mutates startT and therefore I need to be careful
+//     // Renders the slot adding the needed minute to start time to end the slot at 00 minutes of the next hours
+//     timeSlots.push(
+//       `${startT.format("LT")} - ${startT
+//         .add({ minutes: 60 - minutesS })
+//         .format("LT")}`
+//     );
+//   }
 
-  console.log("timeSlots is: ", timeSlots);
-  return timeSlots;
-}
+//   console.log("timeSlots is: ", timeSlots);
+//   return timeSlots;
+// }
 
-// Return an array of dates ranges in the future corresponding to a specified array of days of the week
-// Accept nWeeks param which is used to know which week in the future to use for calculating the date
-// For instance if we want to know the dates of the second week after the current, nWeek will be 2
-const getOneWeekAvailableDates = async (sourceArr, nWeeks) => {
-  const orderDay = moment(Date.now()).add(FulfillmentTime, "h");
-  const todayDay = orderDay.date(); // get current day integer value of the month from date
-  /* currentDayOfWeek needed to prevent rendering an earlier dayoftheWeek.
-  For example, without it if we are on Tuesday, the previous Sunday and Monday would be
-  rendered */
-  const currentDayOfWeek = moment(orderDay).isoWeekday();
+// // Return an array of dates ranges in the future corresponding to a specified array of days of the week
+// // Accept nWeeks param which is used to know which week in the future to use for calculating the date
+// // For instance if we want to know the dates of the second week after the current, nWeek will be 2
+// const getOneWeekAvailableDates = async (sourceArr, nWeeks) => {
+//   const orderDay = moment(Date.now()).add(FulfillmentTime, "h");
+//   const todayDay = orderDay.date(); // get current day integer value of the month from date
+//   /* currentDayOfWeek needed to prevent rendering an earlier dayoftheWeek.
+//   For example, without it if we are on Tuesday, the previous Sunday and Monday would be
+//   rendered */
+//   const currentDayOfWeek = moment(orderDay).isoWeekday();
 
-  return sourceArr
-    .map((item, i) => {
-      // Use js Intl.DateTimeFormat to get the right date format
-      // and moment to get UTC date in millisecons
-      if (item.active && i >= currentDayOfWeek) {
-        const day = new Intl.DateTimeFormat("en-US", {
-          weekday: "long",
-          // year: "numeric",
-          month: "long",
-          day: "2-digit"
-        }).format(
-          moment(orderDay).day(i + nWeeks * 7) // moment().day(24); // 3 Wednesdays from now (3 + 7 + 7 + 7)
-          // .format("dddd, MMMM Do YYYY LT")
-        );
+//   return sourceArr
+//     .map((item, i) => {
+//       // Use js Intl.DateTimeFormat to get the right date format
+//       // and moment to get UTC date in millisecons
+//       if (item.active && i >= currentDayOfWeek) {
+//         const day = new Intl.DateTimeFormat("en-US", {
+//           weekday: "long",
+//           // year: "numeric",
+//           month: "long",
+//           day: "2-digit"
+//         }).format(
+//           moment(orderDay).day(i + nWeeks * 7) // moment().day(24); // 3 Wednesdays from now (3 + 7 + 7 + 7)
+//           // .format("dddd, MMMM Do YYYY LT")
+//         );
 
-        return {
-          locations: item.locations,
-          weekDays: day,
-          // If is current day we need start time to account the Fulfillment time
-          timeSlots: item.times.map((tim, t) => {
-            let dayDay = orderDay.date(); // day Integer value
-            let isCurrentDay = todayDay - dayDay;
-            const currentHours = moment(orderDay).hours(); // HERE WE ADD FULFILLMENT TIME
-            const currentMinutes = moment(orderDay).minutes();
-            const [startTimeHours, minutesS] = tim.startTime.split(":");
+//         return {
+//           locations: item.locations,
+//           weekDays: day,
+//           // If is current day we need start time to account the Fulfillment time
+//           timeSlots: item.times.map((tim, t) => {
+//             let dayDay = orderDay.date(); // day Integer value
+//             let isCurrentDay = todayDay - dayDay;
+//             const currentHours = moment(orderDay).hours(); // HERE WE ADD FULFILLMENT TIME
+//             const currentMinutes = moment(orderDay).minutes();
+//             const [startTimeHours, minutesS] = tim.startTime.split(":");
 
-            // IMPORTANT: Below condition is needed to start rendering only the current day of the week
-            // from the current hour. The condition "currentHours > startTimeHours" instead, is needed to prevent
-            // the first time slot to be rendered twice when times array contains more than one timeslot/obj.
-            // Infact this iteation is called for each timeslot/obj in times array.
-            if (
-              isCurrentDay === 0 &&
-              i === currentDayOfWeek &&
-              currentHours > startTimeHours
-            ) {
-              return getTimeDiff(
-                `${currentHours}:${currentMinutes}`,
-                tim.endTime,
-                day
-              );
-            } else {
-              return getTimeDiff(tim.startTime, tim.endTime, day);
-            }
-          })
-        };
-      }
-      return null;
-    })
-    .filter(elem => elem !== null); // Filter all undefined element due to active being false
-};
-
-// .add(15 * i, 'minutes')
-// .format('hh:mm A');
-const getMultipleWeeksAvailability = async (sourceArr, nWeeks) => {
-  let arr = [];
-  let newArr = [];
-  for (let i = 0; i <= nWeeks; i++) {
-    newArr = await getOneWeekAvailableDates(sourceArr, i);
-    console.log("NEWARR: ", newArr);
-    arr = [...arr, ...newArr];
-  }
-  return await arr;
-};
-
-// const startTime = moment().add(30, "m"); //d = day // m = minutes
-// console.log("startTime is: ", startTime);
-// const endTime = moment()
-//   .add(30, "m")
-//   .day(9); //d = day // m = minutes
-// console.log("endTime is: ", endTime);
-
-// LOAD ARRAY DATA
-// sourceArr = openingHours || deliveryHours
-// nWeeks = the number of weeks to load
-// const getData = async (sourceArr, nWeek) => {
-//   let response = await getMultipleWeeksAvailability(sourceArr, nWeek);
-//   return response;
+//             // IMPORTANT: Below condition is needed to start rendering only the current day of the week
+//             // from the current hour. The condition "currentHours > startTimeHours" instead, is needed to prevent
+//             // the first time slot to be rendered twice when times array contains more than one timeslot/obj.
+//             // Infact this iteation is called for each timeslot/obj in times array.
+//             if (
+//               isCurrentDay === 0 &&
+//               i === currentDayOfWeek &&
+//               currentHours > startTimeHours
+//             ) {
+//               return getTimeDiff(
+//                 `${currentHours}:${currentMinutes}`,
+//                 tim.endTime,
+//                 day
+//               );
+//             } else {
+//               return getTimeDiff(tim.startTime, tim.endTime, day);
+//             }
+//           })
+//         };
+//       }
+//       return null;
+//     })
+//     .filter(elem => elem !== null); // Filter all undefined element due to active being false
 // };
 
-// const HomeDelivery = async props => {
-//   const [time, setTime] = useState([{ locations: "test" }]);
-
-//   return (
-//     <div>
-//       {time.map((loc, x) => (
-//         <Typography key={`x-${x}`}>{loc.locations}</Typography>
-//       ))}
-//     </div>
-//   );
+// // .add(15 * i, 'minutes')
+// // .format('hh:mm A');
+// const getMultipleWeeksAvailability = async (sourceArr, nWeeks) => {
+//   let arr = [];
+//   let newArr = [];
+//   for (let i = 0; i <= nWeeks; i++) {
+//     newArr = await getOneWeekAvailableDates(sourceArr, i);
+//     console.log("NEWARR: ", newArr);
+//     arr = [...arr, ...newArr];
+//   }
+//   return await arr;
 // };
 
 // const currentDayOfWeek = today.getDay();
 export default function App() {
+  // INITIAL STATE
   const [time, setTime] = useState([]);
+
+  // VARIABLES
+  const fulfillmentTime = 0;
+  const nWeeks = 2;
 
   useEffect(() => {
     async function fetchData() {
       console.log("IVOKEDDDDDDDDDD");
-      const result = await getMultipleWeeksAvailability(deliveryHours, 2);
+      const result = await getMultipleWeeksAvailability(
+        deliveryHours,
+        nWeeks, // Int value rapresenting number of weeks in the future to be displayed
+        fulfillmentTime // Int value rapresenting number of hours
+      );
       setTime(result);
     }
 
